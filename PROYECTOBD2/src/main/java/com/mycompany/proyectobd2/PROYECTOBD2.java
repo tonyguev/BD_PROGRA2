d16d2d8cf01b1abf9426a0602073a20bb9f872df
tonyguev <99511829+tonyguev@users.noreply.github.com>
2023-11-27 18:16:33 -0600

    Add files via upload

---
+++ PROYECTOBD2/src/main/java/com/mycompany/proyectobd2/PROYECTOBD2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectobd2;
+
+/**
+ *
+ * @author antho
+ */
+public class PROYECTOBD2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
