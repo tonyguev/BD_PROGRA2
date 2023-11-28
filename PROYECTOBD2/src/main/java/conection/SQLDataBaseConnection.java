907c4b15591a92f29354c2de7d6ccc54a86b4c5b
Anthony Guevara <anthonyguevara2004@gmail.com>
2023-11-28 14:12:38 -0600

    Agregacion con Conexion

---
+++ PROYECTOBD2/src/main/java/conection/SQLDataBaseConnection.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+/**
+ *
+ * @author antho
+ */
+public class SQLDataBaseConnection {
+    private static final String connectionString =
+                "jdbc:sqlserver://DESKTOP-FLV5JB3:1433;"
+                + "database=LIBRERIA;"
+                + "user=sa;"
+                + "password=B1Admin;"
+                + "encrypt=false;"
+                + "trustServerCertificate=false;"
+                + "loginTimeout=30;";
+
+    private SQLDataBaseConnection () {
+    }
+    
+    public static Connection getConnection(){
+        try {
+            Connection connection;
+            connection = DriverManager.getConnection(connectionString);
+            return connection;
+        } catch (SQLException ex) { }
+        return null;
+    }
+        
+        
+        
+}
+
