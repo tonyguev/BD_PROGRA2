99412a3601674e73c8b6d0f804bc01f328137fea
Anthony Guevara <anthonyguevara2004@gmail.com>
2023-11-29 01:58:32 -0600

    AgregoGetHistorico

--- PROYECTOBD2/src/main/java/conection/CorporacionAcces.java
+++ PROYECTOBD2/src/main/java/conection/CorporacionAcces.java
@@ -4,15 +4,51 @@
  */
 package conection;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author antho
  */
 public class CorporacionAcces {
 
-    public static String getHistorico(){
-        
+    public static String getHistorico(String PlantaE, Integer IDEmpleadoE) {
+        String Planta = PlantaE;
+        Integer IDEmpleado = IDEmpleadoE;
+        String Nombre = "";
+        String Apellido = "";
+        String Fecha = "";
+        Float MontoPagadoBruto = 0f;
+        Float MontoPagadoNeto = 0f;
+        try {
+            Connection connection = SQLDataBaseConnection.getConnection();
+            Statement statement = null;
+            statement = connection.createStatement();
 
+            //SELECTSQL STATEMENT
+            String HistorEmple = "EXEC HistoricoEmpleado @IDEmpleado =" + IDEmpleado + ", @Planta = " + Planta;
+            ResultSet resultSet = statement.executeQuery(HistorEmple);
+            resultSet = statement.executeQuery(HistorEmple);
+
+            //Get results from query
+            while (resultSet.next()) {
+                IDEmpleado = resultSet.getInt("IDEmpleado");
+                Nombre = resultSet.getString("Nombre");
+                Apellido = resultSet.getString("Apellido");
+                Fecha = resultSet.getString("Fecha");
+                MontoPagadoBruto = resultSet.getFloat("MontoPagadoBruto");
+                MontoPagadoNeto = resultSet.getFloat("MontoPagadoNeto");
+            }
+            connection.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return IDEmpleado + " " + Nombre + " " + Apellido + " " + Fecha + " " + MontoPagadoBruto + " " + MontoPagadoNeto;
 
     }
     
